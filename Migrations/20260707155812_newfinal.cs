f4b8b078fc335c951881ccb74f1ab625510acd11
Adhil shah <adhilshaht111@gmail.com>
2026-07-15 23:43:15 +0530

    Add migrations for Users table and address column changes
    Added the `20260707155601_new` migration to create the `Users` table with columns `Id`, `Name`, `Email`, `Password`, and `Department`.
    Added the `20260707155812_newfinal` migration to modify the `Users` table by adding an `address` column.
    Added the `20260715180052_revert` migration to revert the addition of the `address` column, restoring the `Users` table to its original state.

---
+++ Migrations/20260707155812_newfinal.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace empService.Migrations
+{
+    /// <inheritdoc />
+    public partial class newfinal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "address",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "address",
+                table: "Users");
+        }
+    }
+}
